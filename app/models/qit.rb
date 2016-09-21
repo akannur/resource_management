7280ca14b9ad2ec469d3519c7b042500ddb31349
Anup <akannur@qwinix.io>
2016-09-21 23:37:48 +0530

    Final fix

--- app/models/qit.rb
+++ app/models/qit.rb
@@ -2,12 +2,12 @@ class Qit < ActiveRecord::Base
   validates :borrower_id, presence: true, uniqueness: true
   validates :borrower, presence: true
   validates :item, presence: true
-  validates :item_id, presence: true
+  validates :item_id, presence: true, uniqueness: true
   validates :borrowed_on, presence: true
   validates :item_status, presence: true
   validates :incharge, presence: true
   validates :issue, presence: true
-  self.per_page = 8
+  self.per_page = 6
   def self.search(search)
     where("item LIKE ?", "%#{search.upcase}%")
   end
